c32e5aefdbee1e8ecba52acfc6628b8ad7092136
HelenArkh <alena.arkh@gmail.com>
2021-10-13 16:43:53 +0300

    Выполнение практического задания

---
+++ WeatherApp/WeatherApp/WeatherApp/GetWeatherPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    public partial class GetWeatherPage : ContentPage
+    {
+        public GetWeatherPage()
+        {
+            InitializeComponent();
+
+            weatherButton.Clicked += Weather_Click;
+        }
+
+        private async void Weather_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WeatherPage());
+        }
+    }
+}
